85c6a863952db92957098f459729ee824d9dc0b0
anuragkumart16 <anuragkumartiwari12@gmail.com>
2024-06-19 06:41:18 +0300

    added changes

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+console.log(typeof document.getElementById('hero').id);
+function RemoveActive() {
+    document.getElementById('navhome').classList.remove('active');
+    document.getElementById('navfeatures').classList.remove('active');
+    document.getElementById('navctasection').classList.remove('active');
+    document.getElementById('navsignup').classList.remove('active');
+}
+
+
 const callback = (entries, observer) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('animatetext');
-        } else {
+        }
+        else {
             entry.target.classList.remove('animatetext');
         }
     })
@@ -18,7 +28,36 @@ const observer = new IntersectionObserver(callback, options);
 
 const targets = document.querySelectorAll('.heading');
 
+
+
 targets.forEach(target => {
     observer.observe(target)
 })
 
+
+const callbacknav =(entries,observerofsection)=>{
+    entries.forEach(entry=>{
+        if (entry.target.id==='hero' && entry.isIntersecting ){
+            RemoveActive()
+            document.getElementById('navhome').classList.add('active');
+        }if(entry.target.id==='ctasection' && entry.isIntersecting){
+            RemoveActive()
+            document.getElementById('navctasection').classList.add('active');
+        }if(entry.target.id==="signupsection" && entry.isIntersecting){
+            RemoveActive()
+            document.getElementById('navsignup').classList.add('active');
+        }if(entry.target.tagName==="P" && entry.isIntersecting){
+            RemoveActive()
+            document.getElementById('navfeatures').classList.add('active');
+        }
+    })
+    
+}
+
+const observerofsection = new IntersectionObserver(callbacknav,options);
+
+const sections = document.querySelectorAll('.navview');
+
+sections.forEach(section => {
+    observerofsection.observe(section)
+})
