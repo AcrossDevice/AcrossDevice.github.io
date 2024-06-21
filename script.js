3fb4e991e5bfdf146e30e6bf061599e2289f731d
anuragkumart16 <anuragkumartiwari12@gmail.com>
2024-06-21 06:36:11 +0300

    front page is about to complete

--- script.js
+++ script.js
@@ -37,26 +37,26 @@ targets.forEach(target => {
 
 
 // code to to update navbar
-const callbacknav =(entries,observerofsection)=>{
-    entries.forEach(entry=>{
-        if (entry.target.id==='hero' && entry.isIntersecting ){
+const callbacknav = (entries, observerofsection) => {
+    entries.forEach(entry => {
+        if (entry.target.id === 'hero' && entry.isIntersecting) {
             RemoveActive()
             document.getElementById('navhome').classList.add('active');
-        }if(entry.target.id==='ctasection' && entry.isIntersecting){
+        } if (entry.target.id === 'ctasection' && entry.isIntersecting) {
             RemoveActive()
             document.getElementById('navctasection').classList.add('active');
-        }if(entry.target.id==="signupsection" && entry.isIntersecting){
+        } if (entry.target.id === "signupsection" && entry.isIntersecting) {
             RemoveActive()
             document.getElementById('navsignup').classList.add('active');
-        }if(entry.target.tagName==="P" && entry.isIntersecting){
+        } if (entry.target.tagName === "P" && entry.isIntersecting) {
             RemoveActive()
             document.getElementById('navfeatures').classList.add('active');
         }
     })
-    
+
 }
 
-const observerofsection = new IntersectionObserver(callbacknav,options);
+const observerofsection = new IntersectionObserver(callbacknav, options);
 
 const sections = document.querySelectorAll('.navview');
 
@@ -67,14 +67,50 @@ sections.forEach(section => {
 
 // scroll to section
 function scrolltoelement(sectionID) {
-    const element = document.getElementById(sectionID);
-    console.log(element);
-    console.log(typeof element);
-    element.scrollIntoView({ behavior: 'smooth' })
+    if (window.innerWidth <= 768) {
+        parameter = false
+        menuopen()
+        const element = document.getElementById(sectionID);
+        console.log(element);
+        console.log(typeof element);
+        element.scrollIntoView({ behavior: 'smooth' })
+    }else{
+        const element = document.getElementById(sectionID);
+        console.log(element);
+        console.log(typeof element);
+        element.scrollIntoView({ behavior: 'smooth' })
+    }
 }
 
+
+
 // use parameters while navigating
-function navwithparam(code){
-    // const encodedData = encodeURIComponent(code);
-    window.location.href='auth.html?data=${code}';
+function navwithparam(code) {
+    const encodedData = encodeURIComponent(code);
+    window.location.href = 'auth.html?data=${encodedData}';
 }
+
+
+// handling the nav bar when device is mobile
+var parameter = true;
+function menuopen() {
+    if (parameter === true) {
+        var header = document.getElementById('header');
+        var menu = document.getElementById('menu');
+        header.style.display = 'block';
+        header.children[1].style.display = 'flex';
+        menu.style.margin = '30px';
+        parameter = false;
+    }
+    else {
+        var header = document.getElementById('header');
+        var menu = document.getElementById('menu');
+        menu.style.margin = '0px';
+        menu.style.marginLeft = 'auto';
+        header.children[1].style.display = 'none';
+        header.style.display = 'flex';
+        parameter = true;
+    }
+}
+
+// funtion to close the navigation bar
